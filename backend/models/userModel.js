87783956bb1b734a832d8ea9d5ed7d1ba9e3bece
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-25 20:07:03 -0300

    Create User Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      require: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      require: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
