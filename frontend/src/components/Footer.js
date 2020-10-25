05778454e9e39e4731836f95f372c9781a23781b
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-25 14:52:18 -0300

    Add react-bootstrap, add some custom classes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; ProShop</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
