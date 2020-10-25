a15a0000b62298e4c9df0c6477471c5975523922
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-25 14:58:28 -0300

    Style header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,34 @@
-import React from "react";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i class="fas fa-user" aria-hidden="true"></i> Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
