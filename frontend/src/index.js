51a33d5511cd33d1bedc153183ec6cbf38fa6d34
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-26 03:19:18 -0300

    Create a Redux Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import "./bootstrap.min.css";
 import App from "./App";
+import store from "./store";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
