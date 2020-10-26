81f62c5ab12a0e1486e7ab585c2a46538b26a02d
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-26 13:59:59 -0300

    errorMiddleware bug fix

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -6,8 +6,7 @@ const notFound = (req, res, next) => {
 
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 400 : res.statusCode;
-  res.status(statusCode);
-  res.json({
+  res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
