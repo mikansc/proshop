0c96fd2164d91f629551b5e9f4acda40d14d440d
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-26 06:02:50 -0300

    Cart Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import "./index.css";
 import "./bootstrap.min.css";
+import "./index.css";
 import App from "./App";
 import store from "./store";
 import reportWebVitals from "./reportWebVitals";
