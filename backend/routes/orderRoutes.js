8d2ce257df3a9ac7f6a4278acf4b12e440fe2ce0
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-27 13:44:38 -0300

    Order Controller & Route

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import orderController from "../controllers/orderController.js";
+import { protect } from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+router.route("/").post(protect, orderController.addOrderItems);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("Server OK"));
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 app.use(errorMiddleware.notFound);
 app.use(errorMiddleware.errorHandler);
 
