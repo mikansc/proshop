24ca1a1f2b6977b9143926b370cedfe25c890a96
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-27 19:54:29 -0300

    Admin User List

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
-                <NavDropdown title={userInfo.name} id="username ">
+                <NavDropdown title={userInfo.name} id="username">
                   <LinkContainer to="/profile">
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
@@ -41,6 +41,19 @@ const Header = () => {
                     <i className="fas fa-user" aria-hidden="true"></i> Sign In
                   </Nav.Link>
                 </LinkContainer>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
               )}
             </Nav>
           </Navbar.Collapse>
---
+++ frontend/src/screens/UserListScreen.js
@@ -0,0 +1,83 @@
+import { useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listUsers } from "../actions/userActions";
+
+const UserListScreen = () => {
+  const dispatch = useDispatch();
+
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
+
+  useEffect(() => {
+    dispatch(listUsers());
+  }, [dispatch]);
+
+  const deleteUserHandler = (id) => {
+    console.log("Delete");
+  };
+
+  return (
+    <>
+      <h1>Users</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped border hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users &&
+              users.map((user) => (
+                <tr key={user.id}>
+                  <th>{user.id}</th>
+                  <th>{user.name}</th>
+                  <th>
+                    <a href={`mailto:${user.email}`}>{user.email}</a>
+                  </th>
+                  <td>
+                    {user.isAdmin ? (
+                      <i
+                        className="fas fa-check"
+                        style={{ color: "green" }}
+                      ></i>
+                    ) : (
+                      <i className="fas fa-times" style={{ color: "red" }}></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/user/${user.id}/edit`}>
+                      <Button variant="light" className="btn-sm">
+                        <i className="fas fa-edit"></i>
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteUserHandler(user._id)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default UserListScreen;
