553b5c47c157ed2921350a4cf3fb96ee50b60c38
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-27 16:24:28 -0300

    Add PayPal API to get Client ID

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,11 @@ app.get("/", (req, res) => res.send("Server OK"));
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
 app.use(errorMiddleware.notFound);
 app.use(errorMiddleware.errorHandler);
 
