8a25449029e33161bb4abf09c690e1a18289d591
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-28 17:49:45 -0300

    Install Morgan

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
@@ -14,6 +15,9 @@ dotenv.config();
 connectDB();
 
 const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
